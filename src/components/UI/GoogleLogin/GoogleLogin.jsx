4de6a55c262829dbf0c37957aa8e2cb0bb7984d8
Andrii Romanovskyi <romanovskyi2137@gmail.com>
2024-07-24 15:50:26 +0300

    upd

--- src/components/UI/GoogleLogin/GoogleLogin.jsx
+++ src/components/UI/GoogleLogin/GoogleLogin.jsx
@@ -1,31 +1,39 @@
 import React, { useEffect } from "react";
-import { getAuth, signInWithRedirect, getRedirectResult } from "firebase/auth";
+import { getAuth, signInWithRedirect, getRedirectResult, signInWithPopup } from "firebase/auth";
 import { app, googleAuthProvider } from '../../../firebase';
 import "./GoogleLogin.css";
 import googleIcon from "./googleIcon.png";
 
 function GoogleLogin ({ onGoogleLogin }) {
     const auth = getAuth(app);
-    useEffect(() => {
-        const handleRedirect = async () => {
-            try {
-                const data = await getRedirectResult(auth);
-                if (data) {
-                    const userData = {
-                        username: data.user.email,
-                        password: data.user.uid
-                    };
-                    await onGoogleLogin(userData);
-                }
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        handleRedirect();
-    }, []);
+    // useEffect(() => {
+    //     const handleRedirect = async () => {
+    //         try {
+    //             const data = await getRedirectResult(auth);
+    //             console.log(data); // why it`s null?
+    //             if (data) {
+    //                 const userData = {
+    //                     username: data.user.email,
+    //                     password: data.user.uid
+    //                 };
+    //                 await onGoogleLogin(userData);
+    //             };
+    //         } catch (error) {
+    //             console.log(error);
+    //         }
+    //     };
+    //     handleRedirect();
+    // }, []);
 
-    const handleGoogleLogin = () => {
-        signInWithRedirect(auth, googleAuthProvider);
+    const handleGoogleLogin = async () => {
+        const data = await signInWithPopup(auth, googleAuthProvider);
+        if (data) {
+            const userData = {
+                username: data.user.email,
+                password: data.user.uid
+            };
+            await onGoogleLogin(userData);
+        };
     };
     
     return (
